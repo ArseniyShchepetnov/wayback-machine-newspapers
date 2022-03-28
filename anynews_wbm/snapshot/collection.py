487d63a6d172f4419d649733698a78d73b9e613e
Arseniy Shchepetnov <a.shchepetnov@gmail.com>
2022-03-28 06:09:02 +0300

    Remove anynews_wbm/snapshot/collection.py

--- anynews_wbm/snapshot/collection.py
+++
@@ -1,8 +0,0 @@
-"""Snapshot collection."""
-from anynews_wbm.snapshot.snapshot import Snapshot
-from typing import List
-
-
-class SnapshotCollection:
-
-    def __init__(self, snapshots: List[Snapshot])
