fc6cd063a018a137e8d2770ea24b211859f32b46
Arseniy Shchepetnov <a.shchepetnov@gmail.com>
2022-09-07 07:22:06 +0300

    Pylint fix

--- wbm_newspapers/waybackmachine/pipelines.py
+++ wbm_newspapers/waybackmachine/pipelines.py
@@ -5,7 +5,6 @@ Don't forget to add your pipeline to the ITEM_PIPELINES setting
 See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 useful for handling different item types with a single interface
 """
-import json
 import logging
 import os
 import shutil
